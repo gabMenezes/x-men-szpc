67b276c07e2571b46bfca5d1a5a676ca10e56f19
Gabriel Menezes <gabrieldmfaquim0202@gmail.com>
2023-07-09 22:34:24 -0300

    responsividade
    Troquei mouseenter por mouseover para funcionar também em mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const personagens = document.querySelectorAll(".personagem");
 
 personagens.forEach((personagem) => {
-  personagem.addEventListener("mouseenter", () => {
+  personagem.addEventListener("mouseover", () => {
 
     if(window.innerWidth < 450) {
       window.scrollTo({top: 0, behavior})
@@ -41,4 +41,3 @@ function removerSelecaoDoPersonagem() {
   const personagemSelecionado = document.querySelector(".selecionado");
   personagemSelecionado.classList.remove("selecionado");
 }
-
